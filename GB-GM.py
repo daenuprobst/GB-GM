a49482f6c07563afd9760052b9b6de9069ea35d8
Daniel Probst <daenuprobst@gmail.com>
2018-11-30 15:05:33 +0100

    Refactored analyize_ZINC.py and GB-GM.py

--- GB-GM.py
+++ GB-GM.py
@@ -1,163 +1,169 @@
-from rdkit import Chem
-from rdkit.Chem import AllChem
+import time
 import random
-import numpy as np
 import pickle
+import numpy as np
+from rdkit.Chem import AllChem
 from io import StringIO
-import sys
-#sio = sys.stderr = StringIO()
 from rdkit import rdBase
+
 rdBase.DisableLog('rdApp.error')
 
-def valences_not_too_large(mol):
-    valence_dict = {5:3, 6:4, 7:3, 8:2, 9:1, 16:6, 17:1, 35:1, 53:1}
-    atomicNumList = [a.GetAtomicNum() for a in mol.GetAtoms()]
-    valences = [valence_dict[atomic_num] for atomic_num in atomicNumList]
-    BO = Chem.GetAdjacencyMatrix(mol,useBO=True)
-    number_of_bonds_list = BO.sum(axis=1)
-    for valence, number_of_bonds in zip(valences,number_of_bonds_list):
-        if number_of_bonds > valence:
-            return False
+random.seed(22)
+np.random.seed(22)
+
+
+class Gene:
+  def __init__(self, size_mean, size_std):
+    # average_size, size_stdev = 23.2, 4.4
+    self.size_mean = size_mean
+    self.size_std = size_std
+
+    self.p_ring = pickle.load(open('p_ring.p', 'rb'))
+    self.rxn_smarts_ring_list = pickle.load(open('rs_ring.p', 'rb'))
+    self.rxn_smarts_list = pickle.load(open('r_s1.p', 'rb'))
+    self.p = pickle.load(open('p1.p', 'rb'))
+
+    prob_double = 0.8
+    self.p_make_ring = self.scale_p_ring(self.p_ring, prob_double)
+
+
+  def run(self, file_name, smiles = 'CC'):
+    with open(file_name, 'w') as f:
+      count = 1
+      while count <= 1000:
+        mol = self.generate_mol(smiles, 50, self.size_mean, self.size_std)
+        if mol:
+          new_smiles = AllChem.MolToSmiles(mol)
+          f.write(new_smiles + '\n')
+          count += 1
+
+  def valences_not_too_large(self, mol):
+    valence_dict = {5: 3, 6: 4, 7: 3, 8: 2, 9: 1, 16: 6, 17: 1, 35: 1, 53: 1}
+    atomic_nums = [a.GetAtomicNum() for a in mol.GetAtoms()]
+    valences = [valence_dict[atomic_num] for atomic_num in atomic_nums]
+    adj = AllChem.GetAdjacencyMatrix(mol, useBO=True)
+
+    n_bonds = adj.sum(axis=1)
+    for valence, n in zip(valences, n_bonds):
+      if n > valence:
+        return False
 
     return True
 
-def run_rxn(rxn_smarts,mol):
-  new_mol_list = []
-  patt = rxn_smarts.split('>>')[0]
-  # work on a copy so an un-kekulized version is returned
-  # if the molecule is not changed
-  mol_copy = Chem.Mol(mol)
-  try:
-    Chem.Kekulize(mol_copy)
-  except:
-    pass
-  if mol_copy.HasSubstructMatch(Chem.MolFromSmarts(patt)):
-    rxn = AllChem.ReactionFromSmarts(rxn_smarts)
-    new_mols = rxn.RunReactants((mol_copy,))
-    for new_mol in new_mols:
-      try:
-        Chem.SanitizeMol(new_mol[0])
-        #new_mol_list.append(Chem.MolFromSmiles(Chem.MolToSmiles(new_mol[0])))
-        new_mol_list.append(new_mol[0])
-      except:
-        pass
-    if len(new_mol_list) > 0:
-      new_mol = random.choice(new_mol_list) 
-      return new_mol
-    else:
-      return mol
-  else:
-      return mol
+  def run_rxn(self, rxn_smarts, mol):
+    new_mol_list = []
+    reactant = rxn_smarts.split('>>')[0]
 
-def add_atom(mol):
-  if np.random.random() < 0.63: # probability of adding ring atom
-    rxn_smarts = np.random.choice(rxn_smarts_ring_list, p=p_ring)
-    if not mol.HasSubstructMatch(Chem.MolFromSmarts('[r3,r4,r5]'))\
-       or AllChem.CalcNumAliphaticRings(mol) == 0:
-      rxn_smarts = np.random.choice(rxn_smarts_make_ring, p=p_make_ring)
-      if np.random.random() < 0.036: # probability of starting a fused ring
-        rxn_smarts = rxn_smarts.replace("!", "")
-  else:
-    if mol.HasSubstructMatch(Chem.MolFromSmarts('[*]1=[*]-[*]=[*]-1')):
-      rxn_smarts = '[r4:1][r4:2]>>[*:1]C[*:2]'
-    else:
-      rxn_smarts = np.random.choice(rxn_smarts_list, p=p)
-    
-  mol = run_rxn(rxn_smarts,mol)
-  smiles = Chem.MolToSmiles(mol)
+    # work on a copy so an un-kekulized version is returned
+    # if the molecule is not changed
+    mol_copy = AllChem.Mol(mol)
 
-  return mol, smiles
+    try:
+      AllChem.Kekulize(mol_copy)
+    except:
+      pass
 
-def expand_small_rings(mol):  
-  Chem.Kekulize(mol,clearAromaticFlags=True)
-  rxn_smarts = '[*;r3,r4;!R2:1][*;r3,r4:2]>>[*:1]C[*:2]'
-  count = 0
-  while mol.HasSubstructMatch(Chem.MolFromSmarts('[r3,r4]=[r3,r4]')):
-    mol = run_rxn(rxn_smarts,mol)
-    #print('expanding ring',Chem.MolToSmiles(mol))
-#    count += 1
-#    if count > 5:
-#    	print('breaking in expand_small_rings')
-#    	sys.exit()
-    
-  return mol
+    if mol_copy.HasSubstructMatch(AllChem.MolFromSmarts(reactant)):
+      rxn = AllChem.ReactionFromSmarts(rxn_smarts)
+      new_mols = rxn.RunReactants((mol_copy,))
 
-def generate_mol(smiles,max_atoms,average_size,size_stdev):
-  #np.random.seed(0)
-  mol = Chem.MolFromSmiles(smiles)
-  num_atoms = mol.GetNumAtoms()
- 
-  target_size = size_stdev*np.random.randn() + average_size
+      for new_mol in new_mols:
+        if AllChem.SanitizeMol(new_mol[0], catchErrors=True) == 0:
+          new_mol_list.append(new_mol[0])
+        else:
+          pass
+      if len(new_mol_list) > 0:
+        new_mol = random.choice(new_mol_list) 
+        return new_mol
 
-  count = 0
-  while num_atoms < max_atoms and count < max_atoms:
-    count += 1
-    mol, smiles = add_atom(mol)
-    num_atoms = mol.GetNumAtoms()
-    if num_atoms > target_size:
-      break
-  
-  if valences_not_too_large(mol):   
-    mol = expand_small_rings(mol)
     return mol
-  else:
-    return None
 
-def scale_p_ring(rxn_smarts_ring_list,p_ring,new_prob_double):
-  p_single = []
-  p_double = []
-  for smarts,p in zip(rxn_smarts_ring_list,p_ring):
-    if '=' in smarts:
-      p_double.append(p)
+  def add_atom(self, mol):
+    rxn_smarts_make_ring = pickle.load(open('rs_make_ring.p','rb'))
+
+    # probability of adding ring atom
+    if np.random.random() < 0.63:
+      rxn_smarts = np.random.choice(self.rxn_smarts_ring_list, p=self.p_ring)
+      smarts = AllChem.MolFromSmarts('[r3,r4,r5]')
+
+      if not mol.HasSubstructMatch(smarts) or AllChem.CalcNumAliphaticRings(mol) == 0:
+        rxn_smarts = np.random.choice(rxn_smarts_make_ring, p=self.p_make_ring)
+
+        # probability of starting a fused ring
+        if np.random.random() < 0.036:
+          rxn_smarts = rxn_smarts.replace("!", "")
     else:
-      p_single.append(p)
-    
-  prob_double, prob_single = sum(p_double), sum(p_single)
-  scale_double = new_prob_double/prob_double
-  scale_single = (1.0 - new_prob_double)/(1-prob_double)
-  for i, smarts in enumerate(rxn_smarts_ring_list):
-    if '=' in smarts:
-      p_ring[i] *= scale_double
-    else:
-      p_ring[i] *= scale_single
+      if mol.HasSubstructMatch(AllChem.MolFromSmarts('[*]1=[*]-[*]=[*]-1')):
+        rxn_smarts = '[r4:1][r4:2]>>[*:1]C[*:2]'
+      else:
+        rxn_smarts = np.random.choice(self.rxn_smarts_list, p=self.p)
       
-  print(scale_double,scale_single*prob_single,sum(p_ring))
+    mol = self.run_rxn(rxn_smarts,mol)
+    smiles = AllChem.MolToSmiles(mol)
+
+    return mol, smiles
+
+  def expand_small_rings(self, mol):  
+    AllChem.Kekulize(mol, clearAromaticFlags=True)
+    rxn_smarts = '[*;r3,r4;!R2:1][*;r3,r4:2]>>[*:1]C[*:2]'
+
+    while mol.HasSubstructMatch(AllChem.MolFromSmarts('[r3,r4]=[r3,r4]')):
+      mol = self.run_rxn(rxn_smarts,mol)
+
+    return mol
+
+  def generate_mol(self, smiles, max_atoms, average_size, size_stdev):
+    mol = AllChem.MolFromSmiles(smiles)
+    num_atoms = mol.GetNumAtoms()
   
-  return p_ring
+    target_size = size_stdev * np.random.randn() + average_size
 
-#########################
+    iteration = 0
+    while num_atoms < max_atoms and iteration < max_atoms:
+      iteration += 1
+      mol, smiles = self.add_atom(mol)
+      num_atoms = mol.GetNumAtoms()
+      if num_atoms > target_size:
+        break
+    
+    if self.valences_not_too_large(mol):   
+      mol = self.expand_small_rings(mol)
+      return mol
+    else:
+      return None
 
-import time
+  def scale_p_ring(self, p_ring, new_prob_double):
+    p_single = []
+    p_double = []
 
-average_size, size_stdev = 23.2, 4.4
-p_ring = pickle.load(open('p_ring.p','rb'))
-p_make_ring = p_ring
-rxn_smarts_make_ring = pickle.load(open('rs_make_ring.p','rb'))
-rxn_smarts_ring_list = pickle.load(open('rs_ring.p','rb'))
+    for smarts, p in zip(self.rxn_smarts_ring_list, p_ring):
+      if '=' in smarts:
+        p_double.append(p)
+      else:
+        p_single.append(p)
+      
+    prob_double = sum(p_double)
+    prob_single = sum(p_single)
+    scale_double = new_prob_double/prob_double
+    scale_single = (1.0 - new_prob_double) / (1 - prob_double)
 
-rxn_smarts_list = pickle.load(open('r_s1.p','rb'))
-p = pickle.load(open('p1.p','rb'))
+    for i, smarts in enumerate(self.rxn_smarts_ring_list):
+      if '=' in smarts:
+        p_ring[i] *= scale_double
+      else:
+        p_ring[i] *= scale_single
+        
+    print(scale_double, scale_single * prob_single, sum(p_ring))
+    
+    return p_ring
 
-prob_double = 0.8
-p_ring = scale_p_ring(rxn_smarts_ring_list,p_ring,prob_double)
-p_make_ring = p_ring
 
 t0 = time.time()
 
-smiles = "CC"
+gene = Gene(23.2, 4.4)
+gene.run('grow1000.smi')
 
-mol_list = []
 
-with open('grow1000.smi','w') as file:
-  count = 1
-  while count <= 1000:
-    mol = generate_mol(smiles,50,average_size,size_stdev)
-    if mol:
-      new_smiles = Chem.MolToSmiles(mol)
-      file.write(new_smiles+'\n')
-      count += 1
-
- 
 t1 = time.time()
 
-print (t1-t0)
+print (t1 - t0)
--- analyze_ZINC.py
+++ analyze_ZINC.py
@@ -1,94 +1,63 @@
-from rdkit import Chem
-from rdkit.Chem import AllChem
-import numpy as np
+import sys
 import pickle
-import operator
-import collections
 import re
+
+import numpy as np
+
+from operator import itemgetter
+from collections import OrderedDict
+from rdkit.Chem import AllChem
 
 def read_file(file_name):
   smiles_list = []
   with open(file_name,'r') as file:
-    for smiles in file:
-      smiles_list.append(smiles)
+    for line in file:
+      smiles_list.append(line.split()[0])
 
   return smiles_list
 
-def get_probs(smarts_list,smiles_list,ring=False):
-  bonds = []
-  probs = collections.OrderedDict()
+def get_probs(smarts_list, smiles, ring=False):
+  probs = OrderedDict()
 
   for smarts in smarts_list:
     probs[smarts] = 0
 
-  number_of_molecules = 0
-  tot = 0
-  for smiles in smiles_list:
-    #print smiles
-    number_of_molecules += 1
-    mol = Chem.MolFromSmiles(smiles)
-    Chem.Kekulize(mol)
+  for s in smiles:
+    mol = AllChem.MolFromSmiles(s)
+    AllChem.Kekulize(mol)
+
     for smarts in smarts_list:
-      matches = mol.GetSubstructMatches(Chem.MolFromSmarts(smarts),uniquify=ring)
-      num_bonds = len(matches) 
-      probs[smarts] += num_bonds
-      tot += num_bonds
-   
-  tot = 0
-  probs2 = collections.OrderedDict()
-  for key in probs:
-    if probs[key] > 0:
-      #print key, probs[key]
-      tot += probs[key]
-      probs2[key] = probs[key]
-            
-  return tot, probs2
+      matches = mol.GetSubstructMatches(AllChem.MolFromSmarts(smarts), uniquify=ring)  
+      probs[smarts] += len(matches)
+  
+  probs = OrderedDict((k, v) for k, v in probs.items() if v > 0)
+  total = sum(probs.values())
+  
+  return total, probs
 
 def clean_probs(probs):
-  exceptions = ['[#7]#','[#8]=','[#9]','[#17]','[#35]','[#53]']
-  probs2 = collections.OrderedDict()
-  # for key in probs:
-  #   skip = False
-  #   for exception in exceptions:
-  #     if exception in key:
-  #       tokens = re.split('\[|\]|;',key)
-  #       alt_key = '['+tokens[3]+']'+tokens[2]+'['+tokens[1]+';!R]'
-  #       probs[alt_key] += probs[key]
-        
-  for key in probs:
-    skip = False
-    for exception in exceptions:
-      if exception in key:
-        skip = True
-    if not skip:
-      probs2[key] = probs[key]
-  
-  tot = 0
-  for key in probs2:
-      tot += probs2[key]
-      
-  return tot, probs2
+  exceptions = ['[#7]#','[#8]=','[#9]','[#17]','[#35]','[#53]']  
+  probs = OrderedDict((k, v) for k, v in probs.items() if not any(substring in k for substring in exceptions))
+  total = sum(probs.values())
 
-  return probs
+  return total, probs
 
 def get_p(probs):
-  p = []
-  for key in probs:
-    p.append(float(probs[key])/tot)
-    
-  return p
+  return np.fromiter(probs.values(), dtype=int) / sum(probs.values())
 
 def get_rxn_smarts_make_rings(probs):
   X = {'[#6R': 'X4', '[#7R': 'X3'}
   rxn_smarts = []
+
   for key in probs:
     tokens = key.split(']')
-
     smarts = ''
+
     if '=' in key:
       smarts += tokens[0][:-1] + X[tokens[0]] + ';!R:1]'
     else:
       smarts += tokens[0][:-1] + ';!R:1]=,'
+
     smarts += tokens[2][:-1] + ';!R:2]>>'
     smarts += '[*:1]1' + tokens[1] + '][*:2]1'
 
@@ -99,6 +68,7 @@ def get_rxn_smarts_make_rings(probs):
 def get_rxn_smarts_rings(probs):
   X = {'[#6R': 'X4', '[#7R': 'X3'}
   rxn_smarts = []
+
   for key in probs:
     tokens = key.split(']')
 
@@ -117,10 +87,12 @@ def get_rxn_smarts_rings(probs):
 
 def get_rxn_smarts(probs):
   rxn_smarts = []
+
   for key in probs:
     smarts = ''
     tokens = key.split(']')
     smarts = tokens[0]
+
     if '-' in key and '#16' not in smarts:  # key <-> smarts
       smarts += ';!H0:1]>>[*:1]'
     if '=' in key and '#16' not in smarts:  # key <-> smarts
@@ -135,170 +107,158 @@ def get_rxn_smarts(probs):
     
   return rxn_smarts
 
-def get_mean_size(smiles_list):
+def get_mean_size(smiles):
   size = []
-  for smiles in smiles_list:
-    mol = Chem.MolFromSmiles(smiles)
-    num_atoms = mol.GetNumAtoms()
-    size.append(num_atoms)
+
+  for s in smiles: 
+    size.append(AllChem.MolFromSmiles(s).GetNumAtoms())
  
   return np.mean(size), np.std(size)
 
-def count_macro_cycles(smiles_list,smarts_list,tot,probs):  
-  #probs = collections.OrderedDict()
-  for smarts in smarts_list:
-    probs[smarts] = 0
-    
-  for smiles in smiles_list:
-    for smarts in smarts_list:
-      mol = Chem.MolFromSmiles(smiles)
-      Chem.Kekulize(mol)
-      matches = mol.GetSubstructMatches(Chem.MolFromSmarts(smarts),uniquify=True)
+def get_macro_cycle_probs(smiles, smarts):
+  probs = OrderedDict()
+
+  for sm in smarts:
+    probs[sm] = 0
+  
+  tot = 0
+  for s in smiles:
+    for sm in smarts:
+      mol = AllChem.MolFromSmiles(s)
+      AllChem.Kekulize(mol)
+      matches = mol.GetSubstructMatches(AllChem.MolFromSmarts(sm), uniquify=True)
       if len(matches) > 0:
-        probs[smarts] += 1
+        probs[sm] += 1
         tot += 1
       
-  return tot,probs
+  return tot, probs
+
+def print_props(smiles_list):
+  smarts = ['[*]','[R]','[!R]','[R2]']
+  mean_size, size_stdv = get_mean_size(smiles_list)
+  print('mean number of non-H atoms' + str(mean_size) + '+/-' + str(size_stdv) + '\n')
 
 
-#################################
+  _ ,probs = get_probs(smarts, smiles_list)
+  print('Probability of ring atoms', float(probs['[R]']) / probs['[*]'])
+  print('Probability of non-ring atoms', float(probs['[!R]']) / probs['[*]'])
+  print('Probability of fused-ring atoms', float(probs['[R2]']) / probs['[*]'])
+  print('')
+
+
+  smarts = ['[R]~[R]~[R]','[R]-[R]-[R]','[R]=[R]-[R]']
+
+  _, probs = get_probs(smarts, smiles_list, ring=True)
+
+  print('Probability of [R]-[R]-[R]', float(probs['[R]-[R]-[R]']) / probs['[R]~[R]~[R]'])
+  print('Probability of [R]=[R]-[R]', float(probs['[R]=[R]-[R]']) / probs['[R]~[R]~[R]'])
+  print('')
+
+
+def pickle_smarts_probs_rings(smiles_list):
+  elements = ['#5','#6','#7','#8','#9','#14','#15','#16','#17','#35','#53']
+
+  smarts = []
+  for element in elements:
+    smarts.append('[' + element + 'R]')
+
+  _, probs_Ratoms = get_probs(smarts, smiles_list)
+
+  R_elements = []
+
+  for key in probs_Ratoms:
+    R_elements.append(key)
+
+  smarts = []
+
+  for i, e1 in enumerate(R_elements):
+    for e2 in R_elements:
+      for j, e3 in enumerate(R_elements):
+        if j >= i:
+          sm_s = e1 + '-' + e2 + '-' + e3
+          if sm_s not in smarts:
+            smarts.append(sm_s)
+        sm_d = e1 + '=' + e2 + '-' + e3
+        if sm_d not in smarts:
+          smarts.append(sm_d)
+
+  tot, probs = get_probs(smarts,smiles_list,ring=True)
+  props_sorted = sorted(probs.items(), key=itemgetter(1), reverse=True)
+
+  for i in range(len(props_sorted)):
+    print (props_sorted[i][0], props_sorted[i][1] / tot)
+
+  print('')
+
+  rxn_smarts_rings = get_rxn_smarts_rings(probs)
+  rxn_smarts_make_rings = get_rxn_smarts_make_rings(probs)
+  p_rings = get_p(probs)
+
+  pickle.dump(p_rings, open('p_ring.p', 'wb')) 
+  pickle.dump(rxn_smarts_rings, open('rs_ring.p', 'wb'))
+  pickle.dump(rxn_smarts_make_rings, open('rs_make_ring.p', 'wb'))
+
+
+
+def pickle_smarts_probs(smiles_list):
+  elements = ['#5','#6','#7','#8','#9','#14','#15','#16','#17','#35','#53']
+  bonds = ['-','=','#']
+
+  smarts = []
+  for bond in bonds:
+    for element1 in elements:
+      for element2 in elements:
+        smarts.append('[' + element1 + ']' + bond + '[' + element2 + ';!R]')
+
+  _, probs = get_probs(smarts,smiles_list)
+  tot, probs = clean_probs(probs)
+
+  p = get_p(probs)
+
+  sorted_props = sorted(probs.items(), key=itemgetter(1), reverse=True)
+
+  for i in range(len(sorted_props)):
+    print(sorted_props[i][0],sorted_props[i][1]/tot)
+
+  rxn_smarts = get_rxn_smarts(probs)
+
+  pickle.dump(p, open('p1.p', 'wb')) 
+  pickle.dump(rxn_smarts, open('r_s1.p', 'wb')) 
+
+
+
+def print_rings(smiles_list):
+  smarts_list = ['[*]1-[*]-[*]-1','[*]1-[*]=[*]-1','[*]1-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-1',
+    '[*]1-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]-1',
+    '[*]1-[*]-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]-[*]-1',
+    '[*]1=[*]-[*]-[*]=[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]=[*]-1']
+
+  smarts_macro = ['[r;!r3;!r4;!r5;!r6;!r8;!r9;!r10;!r11;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r9;!r10;!r11;!r12]',
+    '[r;!r3;!r4;!r5;!r6;!r7;!r8;!r10;!r11;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r11;!r12]',
+    '[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r10;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r10;!r11]']
+
+  total, probs = get_probs(smarts_list, smiles_list, ring=True)
+  t, p = get_macro_cycle_probs(smiles_list, smarts_macro)
+
+  total += t
+  probs.update(p)
+
+  print('')
+  for key in probs:
+    print(key, probs[key])
+
+  print('')
+  print('number of rings', sum(probs.values()))
 
 
 file_name = 'ZINC_first_1000.smi'
 
-elements = ['#5','#6','#7','#8','#9','#14','#15','#16','#17','#35','#53']
-bonds = ['-','=','#']
+if (len(sys.argv) > 1):
+  file_name = sys.argv[1]
 
 smiles_list = read_file(file_name)
 
-
-mean_size, size_stdv = get_mean_size(smiles_list)
-print('mean number of non-H atoms',mean_size,'+/-', size_stdv)
-print('')
-
-smarts = ['[*]','[R]','[!R]','[R2]']
-
-tot,probs = get_probs(smarts,smiles_list)
-print('Probability of ring atoms',float(probs['[R]'])/probs['[*]'])
-print('Probability of non-ring atoms',float(probs['[!R]'])/probs['[*]'])
-print('Probability of fused-ring atoms',float(probs['[R2]'])/probs['[*]'])
-print('')
-
-
-smarts = ['[R]~[R]~[R]','[R]-[R]-[R]','[R]=[R]-[R]']
-
-tot,probs = get_probs(smarts,smiles_list,ring=True)
-
-#print(tot,probs)
-
-print('Probability of [R]-[R]-[R]',float(probs['[R]-[R]-[R]'])/probs['[R]~[R]~[R]'])
-print('Probability of [R]=[R]-[R]',float(probs['[R]=[R]-[R]'])/probs['[R]~[R]~[R]'])
-print('')
-
-smarts = []
-for element in elements:
-  smarts.append('['+element+']')
-
-#print(get_probs(smarts,smiles_list))
-
-smarts = []
-for element in elements:
-  smarts.append('['+element+'R]')
-
-tot_Ratoms,probs_Ratoms = get_probs(smarts,smiles_list)
-#print (tot_Ratoms,probs_Ratoms)
-
-R_elements = []
-for key in probs_Ratoms:
-  R_elements.append(key)
-  
-#print (R_elements)
-
-smarts = []
-
-for i,e1 in enumerate(R_elements):
-  for e2 in R_elements:
-    for j,e3 in enumerate(R_elements):
-      if j >= i:
-        sm_s = e1 + '-' + e2 + '-' + e3
-        if sm_s not in smarts:
-          smarts.append(sm_s)
-      sm_d = e1 + '=' + e2 + '-' + e3
-      if sm_d not in smarts:
-        smarts.append(sm_d)
-        
-#print (len(smarts),smarts)
-
-tot,probs = get_probs(smarts,smiles_list,ring=True)
-#print (tot,probs)
-
-sorted_x = sorted(probs.items(), key=operator.itemgetter(1), reverse=True)
-count = 0
-for i in range(len(sorted_x)):
-  print (sorted_x[i][0],sorted_x[i][1]/tot)
-
-print('')
-#print (count)
-
-rxn_smarts_rings = get_rxn_smarts_rings(probs)
-#print (rxn_smarts_rings)
-
-rxn_smarts_make_rings = get_rxn_smarts_make_rings(probs)
-#print (rxn_smarts_make_rings)
-
-p_rings = get_p(probs)
-#print (p_rings)
-
-pickle.dump(p_rings,open('p_ring.p','wb')) 
-pickle.dump(rxn_smarts_rings,open('rs_ring.p','wb'))
-pickle.dump(rxn_smarts_make_rings,open('rs_make_ring.p','wb'))
-
-smarts = []
-
-for bond in bonds:
-  for element1 in elements:
-    for element2 in elements:
-      smarts.append('['+element1+']'+bond+'['+element2+';!R]')
-
-#print (len(smarts))
-tot,probs = get_probs(smarts,smiles_list)
-tot,probs = clean_probs(probs)
-
-#print (tot, probs)
-p = get_p(probs)
-
-sorted_x = sorted(probs.items(), key=operator.itemgetter(1), reverse=True)
-count = 0
-for i in range(len(sorted_x)):
-  print (sorted_x[i][0],sorted_x[i][1]/tot)
-
-rxn_smarts = get_rxn_smarts(probs)
-#print (rxn_smarts)
-#print (p)
-
-pickle.dump(p,open('p1.p','wb')) 
-pickle.dump(rxn_smarts,open('r_s1.p','wb')) 
-
-
-smarts_list = ['[*]1-[*]-[*]-1','[*]1-[*]=[*]-1','[*]1-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-1',
-          '[*]1-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]-1',
-          '[*]1-[*]-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]-[*]-[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]-[*]-1',
-          '[*]1=[*]-[*]-[*]=[*]-[*]-1','[*]1=[*]-[*]=[*]-[*]=[*]-1']
-
-smarts_macro = ['[r;!r3;!r4;!r5;!r6;!r8;!r9;!r10;!r11;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r9;!r10;!r11;!r12]',
-         '[r;!r3;!r4;!r5;!r6;!r7;!r8;!r10;!r11;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r11;!r12]',
-         '[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r10;!r12]','[r;!r3;!r4;!r5;!r6;!r7;!r8;!r9;!r10;!r11]']
-
-
-tot, probs = get_probs(smarts_list,smiles_list,ring=True)
-
-tot, probs = count_macro_cycles(smiles_list,smarts_macro,tot,probs)
-
-num_rings = 0
-print('')
-for key in probs:
-  print(key,probs[key])
-  num_rings += probs[key]
-
-print('')
-print('number of rings',num_rings)
+print_props(smiles_list)
+pickle_smarts_probs_rings(smiles_list)
+pickle_smarts_probs(smiles_list)
+print_rings(smiles_list)
